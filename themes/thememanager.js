10006764dfb859cb20fd9fd9e0258ec6038c2f1f
ankleBowl <specialreeree6@gmail.com>
2022-09-13 19:50:24 -0700

    i broke everything but maybe thisll fix it

--- themes/thememanager.js
+++ themes/thememanager.js
@@ -3,8 +3,16 @@ var themes = {
     "default": new LangTheme()
 };
 
+// if theme and light are not in localStorage, set them to default and false
+if (localStorage.getItem("theme") == null) {
+    localStorage.setItem("theme", "default");
+}
+if (localStorage.getItem("light") == null) {
+    localStorage.setItem("light", "false");
+}
+
 // Get the current theme from local storage
-var currentTheme = themes[localStorage.getItem("theme") || "default"];
+var currentTheme = themes[localStorage.getItem("theme")];
 var isLight = localStorage.getItem("light") == "true";
 currentTheme.loadTheme(isLight);
 document.body.style.display = "block";
